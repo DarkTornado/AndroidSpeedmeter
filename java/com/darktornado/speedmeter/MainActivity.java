a9554eb24c7b5ab5fb1274e1e6cafcaf410b7661
Dark Tornado <dt3141592@naver.com>
2024-11-05 19:42:12 +0900

    permission requester added

--- java/com/darktornado/speedmeter/MainActivity.java
+++ java/com/darktornado/speedmeter/MainActivity.java
@@ -1,15 +1,20 @@
 package com.darktornado.speedmeter;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    TextView txt;
+    private final String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,34 @@ public class MainActivity extends Activity {
         ScrollView scroll = new ScrollView(this);
         scroll.addView(layout);
         setContentView(scroll);
+
+        if (Build.VERSION.SDK_INT > 23) {
+            if (checkPermission(permissions)) {
+//            startLocationListener();
+            } else {
+                requestPermissions(permissions, 5);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode != 5) return;
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//            startLocationListener();
+        } else {
+            Toast.makeText(this, "위치 권한이 없으면 속도 측정 불가능", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    public boolean checkPermission(String[] permissions) {
+        if (Build.VERSION.SDK_INT < 23) return true;
+        for (String permission : permissions) {
+            if (checkSelfPermission(permission) == PackageManager.PERMISSION_DENIED) return false;
+        }
+        return true;
     }
 
     private int dip2px(int dips) {
